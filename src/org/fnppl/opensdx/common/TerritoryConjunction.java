3b3341eb55e0279b838a97f8170af231ab5019bd
Henning Thieß <thiess@finetunes.net>
2011-02-10 20:40:44 +0100

    some base-classes

--- src/org/fnppl/opensdx/common/TerritoryConjunction.java
+++ src/org/fnppl/opensdx/common/TerritoryConjunction.java
@@ -1,6 +1,31 @@
 package org.fnppl.opensdx.common;
 
 /*
+ * Copyright (C) 2010-2011 
+ * 							fine people e.V. <opensdx@fnppl.org> 
+ * 							Henning Thieß <ht@fnppl.org>
+ * 
+ * 							http://fnppl.org
+ * 
+ * 
+ * This file is part of openSDX
+ * openSDX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * openSDX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * and GNU General Public License along with openSDX.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *      
+ */
+
+/*
  * m49 region-code(s) http://unstats.un.org/unsd/methods/m49/m49regin.htm
  * 
  * HT 20110210 - i think, we should read the m49 and then create *our* list of alpha2-regioncodes for each region
