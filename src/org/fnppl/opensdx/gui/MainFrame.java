bbd367d0be0d916041fcea0279a6bba31194f5be
Henning Thieß <thiess@finetunes.net>
2011-02-10 20:13:49 +0100

    some base-classes

---
+++ src/org/fnppl/opensdx/gui/MainFrame.java
@@ -0,0 +1,12 @@
+package org.fnppl.opensdx.gui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	
+	public static void main(String[] args) {
+		System.out.println("Make me nicy-clicky-bunti!");
+		System.exit(0);
+	}
+
+}
---
+++ src/org/fnppl/opensdx/server/BaseServlet.java
@@ -0,0 +1,14 @@
+package org.fnppl.opensdx.server;
+
+//import javax.servlet.http.*;
+
+//public class BaseServlet extends HttpServlet {
+
+//HT 20110210 hmm, either use the javax.servlet or just write one from scratch - discussion needed.
+
+public class BaseServlet {
+
+//	public static void main(String[] args) {
+//		
+//	}
+}
