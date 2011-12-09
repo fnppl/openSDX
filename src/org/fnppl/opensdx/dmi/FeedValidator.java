2aa8cd3378fe8c43fd43c6bf877098d46bd906c8
Henning Thieß <ht@fnppl.org>
2011-12-09 18:19:48 +0100

    typo

--- src/org/fnppl/opensdx/dmi/FeedValidator.java
+++ src/org/fnppl/opensdx/dmi/FeedValidator.java
@@ -134,6 +134,9 @@ public class FeedValidator {
 	public String validateOSDX_latest(File f) throws Exception { 
 		return validateOSDX_0_0_1(f);
 	}
+	public String validateOSDX_latest(Feed f) throws Exception { 
+		return validateOSDX_0_0_1(f);
+	}
 	
 	public String validateOSDX_0_0_1(String s) { //validate against oSDX 0.0.1 (mayor minor sub)
 		
@@ -153,7 +156,7 @@ public class FeedValidator {
 		return validateXmlFile(f, RESSOURCE_OSDX_0_0_1);
 	}
 	
-	public String validateOSDX_0_1_0(Feed f) { //validate against oSDX 0.1.0 (mayor minor sub)
+	public String validateOSDX_0_0_1(Feed f) { //validate against oSDX 0.1.0 (mayor minor sub)
 		return validateOSDX_0_0_1(f.toElement().toString());  		
 	}
 	
