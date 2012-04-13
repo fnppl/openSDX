183601083c2993bc4022aef9c72f24b268419458
Bertram Boedeker <bboedeker@gmx.de>
2012-04-13 21:24:04 +0200

    OSDX 76 context parameter source file

---
+++ src/org/fnppl/opensdx/file_transfer/trigger/TriggerContext.java
@@ -0,0 +1,14 @@
+package org.fnppl.opensdx.file_transfer.trigger;
+
+public class TriggerContext {
+
+	public final static String USERNAME = "username";
+	public final static String KEYID = "keyid";
+	public final static String RELATED_FILE = "related_file"; //Type File
+	public final static String RELATED_FILENAME = "related_filename"; //Type String	
+	
+	public final static String RELATED_START_DATE = "related_start_date"; //Type long
+	public final static String RELATED_END_DATE = "related_end_date"; 	  //Type long
+	
+	
+}
